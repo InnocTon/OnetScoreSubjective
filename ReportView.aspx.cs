1f8d5276e37a8b929fe9b2b14f97b7f791ce6c68
iOui <iOui@IOUILAB>
2017-02-06 11:38:32 +0700

    update Score To DB Complete

---
+++ ReportView.aspx.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class ReportView : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        ReportBind(TextBox1.Text);
+    }
+    private void ReportBind(string userid)
+    {
+        
+        var rs = new Report1().GetUserReportData(userid);
+
+        ReportViewer1.ProcessingMode = ProcessingMode.Local;
+        ReportViewer1.LocalReport.ReportPath = Server.MapPath("~/App_Code/ReportDataSource/UserReport.rdlc");
+
+        ReportViewer1.LocalReport.DataSources.Add(new Microsoft.Reporting.WebForms.ReportDataSource("UserData", rs));
+        ReportViewer1.LocalReport.Refresh();
+
+        this.ReportViewer1.Visible = true;
+        this.ReportViewer1.ShowBackButton = false;
+        this.ReportViewer1.ShowFindControls = false;
+        this.ReportViewer1.ShowParameterPrompts = false;
+        this.ReportViewer1.ShowPrintButton = true;
+        this.ReportViewer1.ShowZoomControl = true;
+        this.ReportViewer1.ShowRefreshButton = false;
+        ReportViewer1.SizeToReportContent = true;
+        ReportViewer1.ZoomMode = ZoomMode.FullPage;
+        //string deviceInfo =
+        //                     "<DeviceInfo>" +
+        //                     "  <OutputFormat>EMF</OutputFormat>" +
+        //                     "  <PageWidth>21cm</PageWidth>" +
+        //                     "  <PageHeight>29.7cm</PageHeight>" +
+        //                     "  <MarginTop>0.25in</MarginTop>" +
+        //                     "  <MarginLeft>0.25in</MarginLeft>" +
+        //                     "  <MarginRight>0.25in</MarginRight>" +
+        //                     "  <MarginBottom>0.25in</MarginBottom>" +
+        //                     "</DeviceInfo>";
+
+        this.ReportViewer1.DataBind();
+
+
+    }
+}
--- rater31data.aspx.cs
+++ rater31data.aspx.cs
@@ -29,9 +29,19 @@ public partial class rater31data : System.Web.UI.Page
         public string score3_2 { get; set; }
         public string score3_3 { get; set; }
         public string score3_4 { get; set; }
-        public string scoreSum { get; set; }
+        public string sumScore { get; set; }
         public string stdCode { get; set; }
-        
+        public string useScore1_1 { get; set; }
+        public string useScore1_2 { get; set; }
+        public string useScore1_3 { get; set; }
+        public string useScore1_4 { get; set; }
+        public string useScore1_5 { get; set; }
+        public string useScore1_6 { get; set; }
+        public string useScore1_7 { get; set; }
+        public string useScore1_8 { get; set; }
+        public string useScoreSum { get; set; }
+        public string QNO { get; set; }
+
     }
     public class recieveValue32
     {
@@ -39,8 +49,14 @@ public partial class rater31data : System.Web.UI.Page
         public string score1_2 { get; set; }
         public string score2_1 { get; set; }
         public string score2_2 { get; set; }
-        public string scoreSum { get; set; }
+        public string sumScore { get; set; }
         public string stdCode { get; set; }
+        public string useScore1_1 { get; set; }
+        public string useScore1_2 { get; set; }
+        public string useScore1_3 { get; set; }
+        public string useScore1_4 { get; set; }
+        public string useScoreSum { get; set; }
+        public string QNO { get; set; }
 
     }
     [WebMethod(EnableSession = true)]
@@ -57,30 +73,47 @@ public partial class rater31data : System.Web.UI.Page
         decimal score32 = Convert.ToDecimal(query.score3_2.ToString());
         decimal score33 = Convert.ToDecimal(query.score3_3.ToString());
         decimal score34 = Convert.ToDecimal(query.score3_4.ToString());
-        decimal scoreSum = score11 + score12 + score21 + score22 + score31 + score32 + score33 + score34;
-        String studentCode = query.stdCode.ToString(); 
+        decimal scoreSum = Convert.ToDecimal(query.sumScore.ToString());
+        String studentCode = query.stdCode.ToString();
+        string FpaperBarCode = studentCode.Substring(0, 5);
+        string BpaperBarCode = studentCode.Substring(5, 8);
+        string paperBarCode = FpaperBarCode + '3' + BpaperBarCode;
+
+
+        decimal scoreUse11 = Convert.ToDecimal(query.useScore1_1.ToString());
+        decimal scoreUse12 = Convert.ToDecimal(query.useScore1_2.ToString());
+        decimal scoreUse21 = Convert.ToDecimal(query.useScore1_3.ToString());
+        decimal scoreUse22 = Convert.ToDecimal(query.useScore1_4.ToString());
+        decimal scoreUse31 = Convert.ToDecimal(query.useScore1_5.ToString());
+        decimal scoreUse32 = Convert.ToDecimal(query.useScore1_6.ToString());
+        decimal scoreUse33 = Convert.ToDecimal(query.useScore1_7.ToString());
+        decimal scoreUse34 = Convert.ToDecimal(query.useScore1_8.ToString());
+        decimal useScoreSum = Convert.ToDecimal(query.useScoreSum.ToString());
+        decimal QNOuse = Convert.ToDecimal(query.QNO.ToString());
+
         SqlConnection conn = new SqlConnection(connStr);
         SqlTransaction trans = null;
-
-        //SCORE
+        
+        //SCORE Add Score For Tepa No1
         try
         {
 
             conn.Open();
             trans = conn.BeginTransaction();
 
-            SqlCommand command = new SqlCommand("INSERT INTO [TRN_XM_SCORE] ([STD_CODE],[SCORE_TOTAL],[CRITERION1],[CRITERION2],[CRITERION3],[CRITERION4],[CRITERION5],[CRITERION6],[CRITERION7],[CRITERION8],[SCORE_TYPE],[CREATE_BY],[CREATE_DATETIME])  VALUES(@stdCode,@scoreSum,@score11,@score12,@score21,@score22,@score31,@score32,@score33,@score34,@scoreType,@createby,getdate());", conn);
+            SqlCommand command = new SqlCommand("INSERT INTO [TRN_XM_SCORE] ([STD_CODE],[QNO],[SCORE_TOTAL],[CRITERION1],[CRITERION2],[CRITERION3],[CRITERION4],[CRITERION5],[CRITERION6],[CRITERION7],[CRITERION8],[SCORE_TYPE],[CREATE_BY],[CREATE_DATETIME])  VALUES(@stdCode,@QNOuse,@scoreSum,@score11,@score12,@score21,@score22,@score31,@score32,@score33,@score34,@scoreType,@createby,getdate());", conn);
             //command.Parameters.AddWithValue("@stdCode", "2261100001001");
             command.Parameters.AddWithValue("@stdCode", studentCode);
-            command.Parameters.AddWithValue("@scoreSum", scoreSum);
-            command.Parameters.AddWithValue("@score11", score11);
-            command.Parameters.AddWithValue("@score12", score12);
-            command.Parameters.AddWithValue("@score21", score21);
-            command.Parameters.AddWithValue("@score22", score22);
-            command.Parameters.AddWithValue("@score31", score31);
-            command.Parameters.AddWithValue("@score32", score32);
-            command.Parameters.AddWithValue("@score33", score33);
-            command.Parameters.AddWithValue("@score34", score34);
+            command.Parameters.AddWithValue("@QNOuse", QNOuse);
+            command.Parameters.AddWithValue("@scoreSum", useScoreSum);
+            command.Parameters.AddWithValue("@score11", scoreUse11);
+            command.Parameters.AddWithValue("@score12", scoreUse12);
+            command.Parameters.AddWithValue("@score21", scoreUse21);
+            command.Parameters.AddWithValue("@score22", scoreUse22);
+            command.Parameters.AddWithValue("@score31", scoreUse31);
+            command.Parameters.AddWithValue("@score32", scoreUse32);
+            command.Parameters.AddWithValue("@score33", scoreUse33);
+            command.Parameters.AddWithValue("@score34", scoreUse34);
             command.Parameters.AddWithValue("@scoreType", "RATER3");
             //command.Parameters.AddWithValue("@createby", "iOui");
             command.Parameters.AddWithValue("@createby",HttpContext.Current.Session["USER_ID"].ToString());
@@ -104,7 +137,7 @@ public partial class rater31data : System.Web.UI.Page
         {
             trans.Rollback();
             //showMessage("ข้อผิดพลาด!", ex.Message, "error");
-            return new JavaScriptSerializer().Serialize(ex.Message); 
+            //return new JavaScriptSerializer().Serialize(ex.Message); 
 
         }
         finally
@@ -115,8 +148,65 @@ public partial class rater31data : System.Web.UI.Page
             }
         }
 
+        //add to Copy3 No1
+        try
+        {
 
-        //Copy1
+            conn.Open();
+            trans = conn.BeginTransaction();
+
+            SqlCommand command = new SqlCommand("INSERT INTO [TRN_XM_SCORE_COPY3] ([OMR_SEQ],[STD_CODE],[PAPER_BARCODE],[EXAM_KEY],[IS_ROWSMIN],[SCR_CRIT1],[SCR_CRIT2],[SCR_CRIT3],[SCR_CRIT4],[SCR_CRIT5],[SCR_CRIT6],[SCR_CRIT7],[SCR_CRIT8],[SCR_SUM],[QNO],[COPYNO],[IS_DIFF],[IS_COMPLETE],[CREATE_BY],[CREATE_DATETIME])  VALUES(@omrSeq,@stdCode,@paperBarCode,@examKey,@isRowMin,@score11,@score12,@score21,@score22,@score31,@score32,@score33,@score34,@scoreSum,@QNOuse,@copyNo,@isDiff,@isComplete,@createby,getdate());", conn);
+            command.Parameters.AddWithValue("@omrSeq", "");
+            command.Parameters.AddWithValue("@stdCode", studentCode);
+            command.Parameters.AddWithValue("@paperBarCode", paperBarCode);
+            command.Parameters.AddWithValue("@examKey", "");
+            command.Parameters.AddWithValue("@isRowMin", "");
+            command.Parameters.AddWithValue("@scoreSum", useScoreSum);
+            command.Parameters.AddWithValue("@score11", scoreUse11);
+            command.Parameters.AddWithValue("@score12", scoreUse12);
+            command.Parameters.AddWithValue("@score21", scoreUse21);
+            command.Parameters.AddWithValue("@score22", scoreUse22);
+            command.Parameters.AddWithValue("@score31", scoreUse31);
+            command.Parameters.AddWithValue("@score32", scoreUse32);
+            command.Parameters.AddWithValue("@score33", scoreUse33);
+            command.Parameters.AddWithValue("@score34", scoreUse34);
+            command.Parameters.AddWithValue("@QNOuse", QNOuse);
+            command.Parameters.AddWithValue("@copyNo", "3");
+            command.Parameters.AddWithValue("@isDiff", "1");
+            command.Parameters.AddWithValue("@isComplete", "1");
+            command.Parameters.AddWithValue("@createby", HttpContext.Current.Session["USER_ID"].ToString());
+            command.Transaction = trans;
+            int result = command.ExecuteNonQuery();
+            if (result == 1)
+            {
+                trans.Commit();
+                //showMessage("สำเร็จ", "บันทึกข้อมูลการรับ-ส่งกล่องเรียบร้อยแล้ว", "success");
+                //return new JavaScriptSerializer().Serialize(ex.Message);
+            }
+            else
+            {
+                trans.Rollback();
+                //showMessage("ข้อผิดพลาด!", "เกิดความผิดพลาดในการรับกล่อง. กรุณาลองใหม่อีกครั้ง!", "error");
+            }
+
+            conn.Close();
+
+        }
+        catch (Exception ex)
+        {
+            return new JavaScriptSerializer().Serialize(ex.Message);
+            trans.Rollback();
+        }
+        finally
+        {
+            if (conn != null && conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
+        }
+
+
+        //Copy1 Update No1
         try
         {
 
@@ -157,7 +247,7 @@ public partial class rater31data : System.Web.UI.Page
             }
         }
 
-        ////Copy2
+        ////Copy2 Update No1
         try
         {
 
@@ -203,36 +293,49 @@ public partial class rater31data : System.Web.UI.Page
         return new JavaScriptSerializer().Serialize(scoreSum);
     }
 
-    // No2
+    //////***** No2 *****//////
     [WebMethod(EnableSession = true)]
     public static string recieve2(string recieveValue)
     {
-        var query = JsonConvert.DeserializeObject<recieveValue31>(recieveValue);
+        var query = JsonConvert.DeserializeObject<recieveValue32>(recieveValue);
         //string t = query.score3_2;
 
         decimal score11 = Convert.ToDecimal(query.score1_1.ToString());
         decimal score12 = Convert.ToDecimal(query.score1_2.ToString());
         decimal score21 = Convert.ToDecimal(query.score2_1.ToString());
         decimal score22 = Convert.ToDecimal(query.score2_2.ToString());
-        decimal scoreSum = score11 + score12 + score21 + score22;
+        decimal scoreSum = Convert.ToDecimal(query.sumScore.ToString());
+
+        decimal scoreUse11 = Convert.ToDecimal(query.useScore1_1.ToString());
+        decimal scoreUse12 = Convert.ToDecimal(query.useScore1_2.ToString());
+        decimal scoreUse21 = Convert.ToDecimal(query.useScore1_3.ToString());
+        decimal scoreUse22 = Convert.ToDecimal(query.useScore1_4.ToString());
+        decimal useScoreSum = Convert.ToDecimal(query.useScoreSum.ToString());
+        decimal QNOuse = Convert.ToDecimal(query.QNO.ToString());
+
         String studentCode = query.stdCode.ToString();
+        string FpaperBarCode = studentCode.Substring(0, 5);
+        string BpaperBarCode = studentCode.Substring(5, 8);
+        string paperBarCode = FpaperBarCode + '3' + BpaperBarCode;
+
         SqlConnection conn = new SqlConnection(connStr);
         SqlTransaction trans = null;
 
-        //SCORE
+        //SCORE Add For Tepa No2
         try
         {
 
             conn.Open();
             trans = conn.BeginTransaction();
 
-            SqlCommand command = new SqlCommand("INSERT INTO [TRN_XM_SCORE] ([STD_CODE],[SCORE_TOTAL],[CRITERION1],[CRITERION2],[CRITERION3],[CRITERION4],[SCORE_TYPE],[CREATE_BY],[CREATE_DATETIME])  VALUES(@stdCode,@scoreSum,@score11,@score12,@score21,@score22,@scoreType,@createby,getdate());", conn);
+            SqlCommand command = new SqlCommand("INSERT INTO [TRN_XM_SCORE] ([STD_CODE],[QNO],[SCORE_TOTAL],[CRITERION1],[CRITERION2],[CRITERION3],[CRITERION4],[SCORE_TYPE],[CREATE_BY],[CREATE_DATETIME])  VALUES(@stdCode,@QNOuse,@scoreSum,@score11,@score12,@score21,@score22,@scoreType,@createby,getdate());", conn);
             command.Parameters.AddWithValue("@stdCode", studentCode);
-            command.Parameters.AddWithValue("@scoreSum", scoreSum);
-            command.Parameters.AddWithValue("@score11", score11);
-            command.Parameters.AddWithValue("@score12", score12);
-            command.Parameters.AddWithValue("@score21", score21);
-            command.Parameters.AddWithValue("@score22", score22);
+            command.Parameters.AddWithValue("@QNOuse", QNOuse);
+            command.Parameters.AddWithValue("@scoreSum", useScoreSum) ;
+            command.Parameters.AddWithValue("@score11", scoreUse11);
+            command.Parameters.AddWithValue("@score12", scoreUse12);
+            command.Parameters.AddWithValue("@score21", scoreUse21);
+            command.Parameters.AddWithValue("@score22", scoreUse22);
             command.Parameters.AddWithValue("@scoreType", "RATER3");
             command.Parameters.AddWithValue("@createby", HttpContext.Current.Session["USER_ID"].ToString());
             command.Transaction = trans;
@@ -263,8 +366,61 @@ public partial class rater31data : System.Web.UI.Page
             }
         }
 
+        //add to Copy3 No2
+        try
+        {
 
-        //Copy1
+            conn.Open();
+            trans = conn.BeginTransaction();
+
+            SqlCommand command = new SqlCommand("INSERT INTO [TRN_XM_SCORE_COPY3] ([OMR_SEQ],[STD_CODE],[PAPER_BARCODE],[EXAM_KEY],[IS_ROWSMIN],[SCR_CRIT1],[SCR_CRIT2],[SCR_CRIT3],[SCR_CRIT4],[SCR_SUM],[QNO],[COPYNO],[IS_DIFF],[IS_COMPLETE],[CREATE_BY],[CREATE_DATETIME])  VALUES(@omrSeq,@stdCode,@paperBarCode,@examKey,@isRowMin,@score11,@score12,@score21,@score22,@scoreSum,@QNOuse,@copyNo,@isDiff,@isComplete,@createby,getdate());", conn);
+            command.Parameters.AddWithValue("@omrSeq", "");
+            command.Parameters.AddWithValue("@stdCode", studentCode);
+            command.Parameters.AddWithValue("@paperBarCode", paperBarCode);
+            command.Parameters.AddWithValue("@examKey", "");
+            command.Parameters.AddWithValue("@isRowMin", "");
+            command.Parameters.AddWithValue("@scoreSum", useScoreSum);
+            command.Parameters.AddWithValue("@score11", scoreUse11);
+            command.Parameters.AddWithValue("@score12", scoreUse12);
+            command.Parameters.AddWithValue("@score21", scoreUse21);
+            command.Parameters.AddWithValue("@score22", scoreUse22);
+            command.Parameters.AddWithValue("@QNOuse", QNOuse);
+            command.Parameters.AddWithValue("@copyNo", "3");
+            command.Parameters.AddWithValue("@isDiff", "1");
+            command.Parameters.AddWithValue("@isComplete", "1");
+            command.Parameters.AddWithValue("@createby", HttpContext.Current.Session["USER_ID"].ToString());
+            command.Transaction = trans;
+            int result = command.ExecuteNonQuery();
+            if (result == 1)
+            {
+                trans.Commit();
+                //showMessage("สำเร็จ", "บันทึกข้อมูลการรับ-ส่งกล่องเรียบร้อยแล้ว", "success");
+                //return new JavaScriptSerializer().Serialize(ex.Message);
+            }
+            else
+            {
+                trans.Rollback();
+                //showMessage("ข้อผิดพลาด!", "เกิดความผิดพลาดในการรับกล่อง. กรุณาลองใหม่อีกครั้ง!", "error");
+            }
+
+            conn.Close();
+
+        }
+        catch (Exception ex)
+        {
+            return new JavaScriptSerializer().Serialize(ex.Message);
+            trans.Rollback();
+        }
+        finally
+        {
+            if (conn != null && conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
+        }
+
+
+        //Copy1 No2
         try
         {
 
@@ -303,7 +459,7 @@ public partial class rater31data : System.Web.UI.Page
             }
         }
 
-        ////Copy2
+        ////Copy2 No2
         try
         {
 
